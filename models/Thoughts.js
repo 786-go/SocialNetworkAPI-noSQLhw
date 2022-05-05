0bdef272e1c8552fbf4186b10302606d8f0aabfd
786-go <noorullahw@gmail.com>
2022-05-05 12:02:40 -0700

    completed reaction file

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,36 +2,36 @@ const moment = require("moment")
 
 const { Schema, model } = require('mongoose');
 const reactionsSchema = require("./Reaction")
-const formatTime =(timeStamp)=>{
+const formatTime = (timeStamp) => {
     return moment().format("MMM Do YY");
 }
 const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required:true,
-    minlength:1,
-    maxlength: 280
-  },
-  createdAt: {
-    type: Date,
-    default:Date.now,
-    get:timeStamp => formatTime(timeStamp)
-  },
-  username: [
-    {
-      type: String,
-      required:true,
-    }],
-    reactions: [reactionsSchema ]
-},
-{
-    toJSON:{
-        virtuals:true
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
     },
-    id:false
-});
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timeStamp => formatTime(timeStamp)
+    },
+    username:
+    {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionsSchema]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
 
-thoughtSchema.virtual('reactionCount').get(function(){
+thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length
 })
 const Thought = model('Thought', thoughtSchema);
