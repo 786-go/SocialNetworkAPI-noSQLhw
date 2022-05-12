c71bfc8e12a90a3fda42b3ec9deb78497355cec5
786-go <noorullahw@gmail.com>
2022-05-12 11:26:49 -0700

    completed user-routes

--- routes/api_routes/user-routes.js
+++ routes/api_routes/user-routes.js
@@ -1,9 +1,61 @@
 const router = require("express").Router();
+const { request } = require("express");
 const db = require("../../models")
 
-router.get("/api/users", function(req,res) {
-db.User.find({})
-.then(function(records){
-    res.json(records)
+router.get("/api/users", function (req, res) {
+    db.User.find({})
+        .then(function (records) {
+            res.json(records)
+        })
 })
+
+router.get("/api/users/:id", function (req, res) {
+    db.User.findById(req.params.id)
+        .then(function (records) {
+            res.json(records)
+        })
 })
+
+router.post("/api/users/", function (req, res) {
+    db.User.create(req.body)
+        .then(function (records) {
+            res.json(records)
+        })
+})
+
+router.put("/api/users/:id", function (req, res) {
+    db.User.findOneAndUpdate({_id:req.params.id}, {$set:req.body})
+        .then(function (records) {
+            res.json(records)
+        })
+})
+
+router.delete("/api/users/:id", function (req, res) {
+    db.User.findOneAndDelete({_id:req.params.id})
+        .then(function (records) {
+            res.json(records)
+        })
+})
+
+//update
+router.post("/api/users/:userId/friends/:friendId", function (req, res) {
+    db.User.findOneAndUpdate({_id:req.params.userId},
+         {addToSet:{friends:req.params.friendId}},
+         {new:true}) 
+        .then(function (records) {
+            res.json(records)
+        })
+})
+
+//update
+
+router.delete("/api/users/:userId/friends/:friendId", function (req, res) {
+    db.User.findOneAndUpdate({_id:req.params.userId},
+         {addToSet:{friends:req.params.friendId}},
+         {new:true}) 
+        .then(function (records) {
+            res.json(records)
+        })
+})
+
+module.exports = router
